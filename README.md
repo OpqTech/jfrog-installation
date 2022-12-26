42095ff30a7387eb8d790df4be69eece3143a47b
Opqtech <optrainings@opqbootcamp.com>
2022-12-26 14:48:48 +0530

    updated

---
+++ README.md
@@ -0,0 +1,28 @@
+# jfrog-installation-ubuntu
+
+
+## Shell script to pull latest artifact
+```
+#!/bin/bash
+
+# Artifactory location
+ip=$1
+server=ip/artifactory
+repo=$2
+
+# Maven artifact location
+name=$3
+artifact=$4/$name
+path=$server/$repo/$artifact
+version=$(curl -s $path/maven-metadata.xml | grep latest | sed "s/.*<latest>\([^<]*\)<\/latest>.*/\1/")
+build=$(curl -s $path/$version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
+war=$name-$build.war
+url=$path/$version/$war
+
+# Download
+echo $url
+wget $url
+```
+
+### Sample Usage [https://github.com/opqtech/sonarqube-example]
+```
---
+++ install-jfrog-ubuntu.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "\n################################################################"
+echo "#                                                              #"
+echo "#                     ***OpqTech***                            #"
+echo "#                 Artifactory  Installation                    #"
+echo "#                                                              #"
+echo "################################################################"
+
+# Installing necessary packages
+echo "\n\n*****Installing necessary packages"
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get install -y default-jre unzip > /dev/null 2>&1
+echo "            -> Done"
+
+# Configuring Artifactory as a Service
+echo "*****Configuring Artifactory as a Service"
+sudo useradd -r -m -U -d /opt/artifactory -s /bin/false artifactory 2>/dev/null
+sudo cp artifactory.service /etc/systemd/system/artifactory.service
+sudo systemctl daemon-reload 1>/dev/null
+echo "            -> Done"
+
+# Downloading JFROG Artifactory 6.9.6 version to OPT folder
+echo "*****Downloading JFROG Artifactory 6.9.6 version"
+sudo systemctl stop artifactory > /dev/null 2>&1
+cd /opt 
+sudo rm -rf jfrog* artifactory*
+sudo wget -q https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.6.zip
+sudo unzip -q jfrog-artifactory-oss-6.9.6.zip -d /opt/artifactory 1>/dev/null
+sudo chown -R artifactory: /opt/artifactory/*
+sudo rm -rf jfrog-artifactory-oss-6.9.6.zip
+echo "            -> Done"
+
+# Starting Artifactory Service
+echo "*****Starting Artifactory Service"
+sudo systemctl start artifactory 1>/dev/null
+
+
+# Check if Artifactory is working
+sudo systemctl is-active --quiet artifactory
+echo "\n################################################################ \n"
+if [ $? -eq 0 ]; then
+	echo "Artifactory installed Successfully"
+	echo "Access Artifactory using $(curl -s ifconfig.me):8081"
+else
+	echo "Artifactory installation failed"
+fi
+echo "\n################################################################ \n"
+
